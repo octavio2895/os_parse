b04003ca16fe601ed55408e146f3c8be623c76ff
Octavio Echeverria <octavio2895@gmail.com>
2021-09-03 13:32:36 -0500

    wip

--- eplus_db.py
+++ eplus_db.py
@@ -4,12 +4,18 @@ class Zone():
     '''
     Abstracts zone information
     '''
+    def get_timed_data(self):
+        print(self.timed_data)
 
-    def __init__(self):
+    def __init__(self, zone_data=None):
+        self.zone_data = zone_data #TODO assure this is an array
+        self.zone_name = zone_data[0][4]
+        self.timed_data = [row[2] for row in zone_data] 
+        self.timed_data_len = len(self.timed_data)
         return
 
     def __str__(self):
-        return(self.zone_name)
+        return("EnergyPlus Zone: %s\nNumber of entries: %d" % (self.zone_name, self.timed_data_len))
         
 class EPlusDB():
     '''
@@ -19,7 +25,7 @@ class EPlusDB():
     def query_zone_info(self, zone_name):
 
         cursor = self.con.cursor()
-
+        print("Requesting data for %s..." % zone_name)
         cursor.execute("""SELECT ReportData.ReportDataDictionaryIndex, ReportData.TimeIndex, ReportData.Value, ReportDataDictionary.ReportDataDictionaryIndex, ReportDataDictionary.KeyValue, ReportDataDictionary.Name, Time.TimeIndex, Time.DayType
         FROM ReportData
         JOIN ReportDataDictionary
@@ -31,6 +37,13 @@ class EPlusDB():
         timed_data = cursor.fetchall()
         return timed_data 
 
+    def get_all_zones_data(self):
+        self.query_zones_names()
+        print("Requested zones: %d" % self.zones_len) #TODO Implement a logger
+        for name in self.zones_names:
+            self.zones.append(Zone(self.query_zone_info(name)))
+        return  
+
     def query_zones_names(self):
         '''
         Queries the DB for the zones, returns the zones as a list.
@@ -38,7 +51,7 @@ class EPlusDB():
         if self.is_connected:
             cursor = self.con.cursor()
             cursor.execute("select * from zones") 
-            self.zones_names = cursor.fetchall()
+            self.zones_names = [row[1] for row in cursor.fetchall()]
             self.zones_len = len(self.zones_names)
             return self.zones_names
         else:
@@ -63,6 +76,7 @@ class EPlusDB():
         self.db_path = None
         self.zones_len = 0
         self.zones_names = None
+        self.zones = []
 
     def __str__(self):
         #TODO be more descriptive
@@ -70,7 +84,13 @@ class EPlusDB():
 
 eplusdb = EPlusDB()
 eplusdb.connect_db("db/eplusout.sql")
+eplusdb.get_all_zones_data()
+for zone in eplusdb.zones:
+    print(zone)
+'''
 names = eplusdb.query_zones_names()
 print(names[0][1])
-zone1 = eplusdb.query_zone_info(names[0][1])
+zone1 = Zone(eplusdb.query_zone_info(names[0][1]))
 print(zone1)
+#zone1.get_timed_data()
+'''
